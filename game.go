24076c874b056ed9fc1ac90def60d1cdd49cb07a
Naroxija <szalayerik14@gmail.com>
2026-02-05 18:35:46 +0200

    Update game.go

--- game.go
+++ game.go
@@ -46,3 +46,59 @@ func Play(scanner *bufio.Scanner, secret string) (int, bool) {
 	fmt.Println(secret)
 	return attempts, false
 }
+
+
+
+
+func initRemaining() map[rune]bool {
+	m := make(map[rune]bool)
+	for c := 'A'; c <= 'Z'; c++ {
+		m[c] = true
+	}
+	return m
+}
+
+func remainingString(m map[rune]bool) string {
+	var letters []string
+	for c, ok := range m {
+		if ok {
+			letters = append(letters, string(c))
+		}
+	}
+	sort.Strings(letters)
+	return strings.Join(letters, " ")
+}
+
+func evaluate(secret, guess string, remaining map[rune]bool) string {
+	result := make([]string, 5)
+	used := make([]bool, 5)
+
+	for i := 0; i < 5; i++ {
+		if guess[i] == secret[i] {
+			result[i] = green + string(guess[i]) + reset
+			used[i] = true
+		}
+	}
+
+	for i := 0; i < 5; i++ {
+		if result[i] != "" {
+			continue
+		}
+		found := false
+		for j := 0; j < 5; j++ {
+			if !used[j] && guess[i] == secret[j] {
+				found = true
+				used[j] = true
+				break
+			}
+		}
+		if found {
+			result[i] = yellow + string(guess[i]) + reset
+		} else {
+			result[i] = white + string(guess[i]) + reset
+			remaining[rune(guess[i])] = false
+		}
+	}
+
+	return strings.Join(result, "")
+}
